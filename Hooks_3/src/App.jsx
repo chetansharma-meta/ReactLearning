ae750abf49a869a93e09f53d8b19ef76d6305f43
Chetan Sharma <66529151+chetan-sharma-g@users.noreply.github.com>
2024-01-02 16:05:34 +0530

    In Hooks Project upper limit and lower limit of the setCounter is added at 0 and 20 respectively

--- Hooks_3/src/App.jsx
+++ Hooks_3/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react';
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  let [counter,setCounter]  = useState(0);
+  let [counter, setCounter] = useState(0);
 
   const addValue = () => {
-    setCounter(counter + 1);
-    console.log(counter);
-  }
+    if (counter < 20) {
+      setCounter(counter + 1);
+      console.log(counter);
+    }
+  };
   const decValue = () => {
-    setCounter(counter - 1);
-    console.log(counter);
-  }
+    if (counter > 0) {
+      setCounter(counter - 1);
+      console.log(counter);
+    }
+  };
 
   return (
     <>
-     <h1>chetan sharma</h1>
+      <h1>chetan sharma</h1>
 
-     <h2>Value counter: {counter}</h2>
+      <h2>Value counter: {counter}</h2>
 
-     <button 
-      onClick={addValue}
-     >Add Val</button>
-     <br />
-     <br />
-     <button 
-      onClick={decValue}
-     >Remove Val</button>
+      <button onClick={addValue}>Add Val</button>
+      <br />
+      <br />
+      <button onClick={decValue}>Remove Val</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
